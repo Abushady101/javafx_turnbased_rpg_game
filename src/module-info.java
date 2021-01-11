d7e6770372476392f79f1a00d313128a75fb825b
Mazen Mamdouh <69313203+MazenMamdouh@users.noreply.github.com>
2021-01-11 15:42:37 +0200

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module Final {
+    requires java.sql;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires mysql.connector.java;
+    requires java.desktop;
+    requires javafx.media;
+    opens sample;
+}
